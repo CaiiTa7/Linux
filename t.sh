f3b95650481db22f76f5c8ace7a44d84345b7d61
CaiiTa7 <98960052+CaiiTa7@users.noreply.github.com>
2024-01-22 23:07:39 +0100

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,3 +1,66 @@
+#!/bin/bash
+pacman -Syu --noconfirm make gcc wget curl git base-devel net-tools neofetch zsh unzip
+
+url="https://www.softether-download.com/files/softether/"
+get_last_version() {
+    # Effectuer une requête HTTP pour obtenir le contenu de la page
+    response=$(curl -s "$1")
+    
+    if [ $? -eq 0 ]; then
+        # Filtrer les liens pour obtenir les versions et les trier
+        versions=$(echo "$response" | grep -oP 'v\d+\.\d+-\d+-\w+-\d+\.\d+\.\d+-tree')
+        
+        if [ -n "$versions" ]; then
+            # Retourner la dernière version
+            echo "$versions" | sort -r | head -n 1
+            return
+        fi
+    fi
+    echo "Nope"
+}
+version=$(get_last_version "$url")
+last_version=$(echo "$version" | sed 's/-tree/-linux-x64-64bit.tar.gz/')
+download_link="$url$version/Linux/SoftEther_VPN_Client/64bit_-_Intel_x64_or_AMD64/softether-vpnclient-$last_version"
+package_name="softether-vpnclient-$last_version"
+if [ "$last_version" != "Nope" ]; then
+    mkdir -p /opt/softether
+    cd /opt/softether
+    echo "Downloading SoftEther VPN Client..."
+    wget -q "$download_link"
+    echo "Extracting..."
+    tar -xzvf "$package_name"
+    echo "Installing..."
+    cd vpnclient
+    make
+    cd /opt/softether/vpnclient
+    make install
+    
+    echo "Cleaning..."
+    rm -rf "$package_name"
+    echo "Done"
+fi
+VPNCLIENT_BIN="./vpnclient"
+VPNCMD_BIN="./vpncmd"
+HOSTNAME="networking.iesn.henallux.be"
+PORT="443"
+HUB_NAME="ETU"
+USER_NAME="MASI_ETU_LAB"
+PASSWORD="PwD_M@s1-L@b0"
+$VPNCLIENT_BIN start
+$VPNCMD_BIN localhost /CLIENT /CMD NicCreate "VPN"
+# Créer une nouvelle connexion nommée "MyConnection"
+$VPNCMD_BIN localhost /CLIENT /CMD AccountCreate MyConnection /SERVER:$HOSTNAME:$PORT /HUB:$HUB_NAME /USERNAME:$USER_NAME /NICNAME:VPN
+# Co$nfigurer la connexion pour l'authentification par mot de passe
+$VPNCMD_BIN localhost /CLIENT /CMD AccountPasswordSet MyConnection /PASSWORD:$PASSWORD /TYPE:standard
+# Dé$marrer la connexion VPN
+$VPNCMD_BIN localhost /CLIENT /CMD AccountConnect MyConnection
+# Vé$rifier l'état de la connexion
+$VPNCMD_BIN localhost /CLIENT /CMD AccountStatusGet MyConnection
+sleep 3
+config_file="~/.zshrc"
+echo '
+eval "$(oh-my-posh --init --shell zsh --config ~/.poshthemes/dracula.omp.json)" 
+
 start_vpn() {
     dev="vpn_vpn"
     sudo /opt/softether/vpnclient/vpnclient start
@@ -15,12 +78,11 @@ start_vpn() {
     fi
 }
 
-config_file="~/.zshrc"
-
-echo 'alias vpn_start=start_vpn' >> $config_file
-echo 'alias vpn_stop="sudo /opt/softether/vpnclient/vpnclient stop"' >> $config_file
-echo 'alias vpn_restart="sudo /opt/softether/vpnclient/vpnclient stop && sudo /opt/softether/vpnclient/vpnclient start"' >> $config_file
-echo 'alias vpn_status="sudo /opt/softether/vpnclient/vpnclient status"' >> $config_file
+alias vpn_start=start_vpn
+alias vpn_stop="sudo /opt/softether/vpnclient/vpnclient stop"
+alias vpn_restart="sudo /opt/softether/vpnclient/vpnclient stop && sudo /opt/softether/vpnclient/vpnclient start"
+alias vpn_status="sudo /opt/softether/vpnclient/vpnclient status"
+' >> $config_file
 
 echo "Fini"
 echo "Les alias VPN ont été ajoutés à $config_file"
@@ -44,8 +106,6 @@ chmod u+rw ~/.poshthemes/*.json
 rm ~/.poshthemes/themes.zip
 
 # Configurer Oh My Posh avec le thème Dracula
-
-eval "$(oh-my-posh --init --shell zsh --config ~/.dracula.omp.json)"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Meslo.zip -O ~/Downloads/Meslo.zip
 cd ~
 mkdir .fonts
